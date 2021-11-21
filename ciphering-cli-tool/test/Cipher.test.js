cbf190863ce66ace106053e5c5d3f158ded585c5
KononOleg <konon.oleg97@gmail.com>
2021-11-21 17:43:01 +0300

    feat: add parseConfig test

--- ciphering-cli-tool/test/Cipher.test.js
+++ ciphering-cli-tool/test/Cipher.test.js
@@ -3,26 +3,26 @@ const { cipherCaesar, cipherAtbash, cipherROT } = require("../src/Cipher/Cipher"
 describe("Cipher:", () => {
   test("Cipher Caesar encode", () => {
     const res = cipherCaesar("A", 1);
-    expect(res).toEqual("B");
+    expect(res).toBe("B");
   });
   test("Cipher Atbash encode", () => {
     const res = cipherAtbash("A");
-    expect(res).toEqual("Z");
+    expect(res).toBe("Z");
   });
   test("Cipher Rot8", () => {
     const res = cipherROT("A", 1);
-    expect(res).toEqual("I");
+    expect(res).toBe("I");
   });
   test("Cipher Rot8 decode", () => {
     const res = cipherROT("A", 0);
-    expect(res).toEqual("S");
+    expect(res).toBe("S");
   });
   test("Cipher Caesar encode lower case ", () => {
     const res = cipherCaesar("a", 1);
-    expect(res).toEqual("b");
+    expect(res).toBe("b");
   });
   test("Cipher Caesar encode not a letter ", () => {
     const res = cipherCaesar("_", 1);
-    expect(res).toEqual("_");
+    expect(res).toBe("_");
   });
 });
